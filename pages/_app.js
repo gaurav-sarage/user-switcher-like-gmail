c2ce80123b1193e15b444e635c503fa3c76d9f45
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-02 17:18:10 +0530

    resolved a bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,50 @@
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
+
 import { useRouter } from "next/router";
+
 import Head from "next/head";
+
 import Link from "next/link";
 
-// unprotected routes (pages that can be accessed without an active user session)
+// pages that can be accessed without an active user session.
 
-const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[///index]]"];
+const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[...index]]"];
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
 
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider {...pageProps} publishableKey={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API} >
       <Head>
-        <title>User Switching App using Clerk</title>
+        <title>Clerk app</title>
+
         <link rel="icon" href="/favicon.ico" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      {/*If the route is for home, sign-in or sign-up: show them without checks*/}
+      {/*If the route is for the home, sign-in and sign-up pages: show them without checks.*/}
 
       {publicPages.includes(router.pathname) ? (
         <Component {...pageProps} />
       ) : (
         <>
+          {/*Show all pages if the user is signed in*/}
+
           <SignedIn>
             <Component {...pageProps} />
           </SignedIn>
+
+          {/*Ask to sign in if the user isn't signed in*/}
 
           <SignedOut>
             <main>
               <p>
                 Please{" "}
                 <Link href="/sign-in">
-                  <a>Sign In</a>
+                  <a>sign in</a>
                 </Link>{" "}
-                to access the page
+                to access this page.
               </p>
             </main>
           </SignedOut>
