289bd75bae6a177a247448790edb2f9aecdc3dd7
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-02 16:55:29 +0530

    created a component to access the public route for the user

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from "next/link";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// unprotected routes (pages that can be accessed without an active user session)
+
+const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[///index]]"];
+
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  return (
+    <ClerkProvider {...pageProps}>
+      <Head>
+        <title>User Switching App using Clerk</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+    </ClerkProvider>
+  )
 }
-
-export default MyApp
